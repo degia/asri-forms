dd9495d79c557e2bb162751537e2b3e992f9a3a3
Degia Parlopa Pasaribu <degia56.dp@gmail.com>
2026-08-12 21:54:47 +0700

    Login: tambah footer © 2026 Nuvista

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,6 +23,7 @@
                     <p class="mt-1 text-sm text-muted">IT Department &mdash; ASRI</p>
                 </div>
                 {{ $slot }}
+                <p class="mt-8 text-center text-xs text-muted">&copy; 2026 Nuvista</p>
             </div>
         </div>
     </body>
